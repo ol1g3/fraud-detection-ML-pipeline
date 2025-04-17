dac7135cdcb18044517a0b7ccb11ff83694aa4d1
oleh <olegprogir@gmail.com>
2025-04-17 23:36:12 +0200

    minor changes

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -2,7 +2,6 @@ import sys
 import os
 import tempfile
 
-print(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 import pandas as pd
